da533709193ede88e7687cac3210a1db90e0265b
skanda-vasishta <124769936+skanda-vasishta@users.noreply.github.com>
2023-10-10 17:04:58 -0700

    Update tokens.h with basic struct

--- src/lib/tokens.h
+++ src/lib/tokens.h
@@ -1,1 +1,13 @@
+#include <iostream>
 
+struct Tokens {
+  int line;
+  int col;
+  std::string text;
+
+  Tokens(int line, int col, std::string text) {
+    this->line = line;
+    this->col = col;
+    this->text = text;
+  }
+};
