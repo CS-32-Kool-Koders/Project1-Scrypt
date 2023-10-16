551c74735581be5d1f73a20def22c5164b67f4ec
Skanda Vasishta <skandavasishta@ucsb.edu>
2023-10-16 14:58:13 -0700

    fuck C++

--- src/parse.cpp
+++ src/parse.cpp
@@ -205,7 +205,7 @@ int main()
     // }
  
     if (Lexer.tokenList.back().text != "END") {
-        std::cout << std::endl << "Unexpected token at line " << Lexer.tokenList.back().line
+        std::cout  << "Unexpected token at line " << Lexer.tokenList.back().line
                 << " column " << Lexer.tokenList.back().col << ": "
                 << Lexer.tokenList.back().text << std::endl;
         exit(2);
