31ae999438eb6bccc861f79c6e6cdf5bc0e08cb6
Bharat Saiju <bsaiju@cs.ucsb.edu>
2023-10-26 03:31:25 -0700

    this should fix the parenthesis problem? i hope

--- src/calc.cpp
+++ src/calc.cpp
@@ -31,28 +31,31 @@ int main()
                 std::istringstream stream(line);
                 lexer.tokenize(1, stream.str());
                 lexer.tokenList.push_back(Tokens(1, lexer.tokenList.back().col + 1, "END"));
+
+
                 int paren_count = 0;
                 //int offset = 0;
-                for(size_t i =0; i < lexer.tokenList.size(); i++) {
-                    std::cout << "Token at " << i << " " << lexer.tokenList[i].text;
-                    if(lexer.tokenList[i].text == "(") {
+                std::string tokenlist = stream.str();
+                for(size_t i =0; i < tokenlist.length(); i++) {
+                    //std::cout << "Token at " << i << " " << tokenlist[i];
+                    if(tokenlist[i] == '(') {
                         paren_count++;
                     }
-                    else if(lexer.tokenList[i].text == ")") {
+                    else if(tokenlist[i] == ')') {
                         paren_count--;
                     }
-                    int int_listSize = lexer.tokenList.size();
-                    if(lexer.tokenList[i].text == "END" && i != (size_t)int_listSize-1) {
-                        std::cout << "the end";
-                        std::string throw_message = "Unexpected token at line 1 column " + std::to_string(i) + ": " + lexer.tokenList[i].text;
+                    int int_listSize = tokenlist.length();
+                    if(tokenlist.substr(i,i+2) == "END" && i != (size_t)int_listSize-1) {
+                        //std::cout << "the end";
+                        std::string throw_message = "Unexpected token at line 1 column " + std::to_string(i) + ": " + tokenlist.substr(i, i+2);
                         throw std::logic_error(throw_message);
                     }
                     else if((paren_count < 0) || (paren_count == 0 && (i < (size_t) int_listSize-2) && (i >0)) || paren_count > int_listSize-1-(int)i){
-                        std::cout << "Paren count " << paren_count <<std::endl;
-                        std::cout << "i " << i << std::endl;
-                        // //std::cout << "offset " << offset << std::endl;
-                        std::cout << "int_listSize " << int_listSize << std::endl;
-                        std::string throw_message = "Unexpected token at line 1 column " + std::to_string(i) + ": " + lexer.tokenList[i].text;
+                        // std::cout << "Paren count " << paren_count <<std::endl;
+                        // std::cout << "i " << i << std::endl;
+                        // // //std::cout << "offset " << offset << std::endl;
+                        // std::cout << "int_listSize " << int_listSize << std::endl;
+                        std::string throw_message = "Unexpected token at line 1 column " + std::to_string(i) + ": " + tokenlist[i];
                         throw std::logic_error(throw_message);
                     }
                     // else {
@@ -60,6 +63,8 @@ int main()
                     //     throw std::logic_error(throw_message);
                     // }
                 }
+
+                
                 // if(paren_count != 0) {
                 //     std::string throw_message = "Unexpected token at line 1 column " + std::to_string(lexer.tokenList.back().col) + ": " + lexer.tokenList.back().text;
                 //     throw std::logic_error(throw_message);
