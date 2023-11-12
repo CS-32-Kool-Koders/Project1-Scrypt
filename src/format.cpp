8121dcfaa09b590cf8c2ae1cea9b261d179ba770
EnergyCube <dev@energycube.fr>
2023-11-11 16:23:40 -0800

    feat: compute infix before printing

--- src/format.cpp
+++ src/format.cpp
@@ -10,46 +10,51 @@
 // Assuming 'Tokens', 'Blocks', and other necessary classes are defined in the included headers
 
 // Forward declarations
-void printAST(const Blocks* block, int indent = 0);
-Blocks* buildAST(const std::vector<Tokens>& /* tokens */);
-void printExpression( ExpressionParser* exp/* expr */);
+void printAST(const Blocks *block, int indent = 0);
+Blocks *buildAST(const std::vector<Tokens> & /* tokens */);
+void printExpression(ExpressionParser *exp /* expr */);
 bool checkoperator(std::string op);
-Blocks* parseStatements(std::vector<std::vector<Tokens>>& lines, size_t& lineIndex, size_t& tokenIndex);
-Blocks* parseBlock(std::vector<std::vector<Tokens>>& lines, size_t& lineIndex, size_t& tokenIndex);
+Blocks *parseStatements(std::vector<std::vector<Tokens>> &lines, size_t &lineIndex, size_t &tokenIndex);
+Blocks *parseBlock(std::vector<std::vector<Tokens>> &lines, size_t &lineIndex, size_t &tokenIndex);
 // parsestatements and parseblock will eventually have to be fixed. for now let it be a placeholder for constructing the AST
 
-
-int main() {
-    std::string line; 
-    int row = 0; 
+int main()
+{
+    std::string line;
+    int row = 0;
     lexer Lexer;
-    int new_line = 0; 
-    try{
-        while(!std::cin.eof()) {
+    int new_line = 0;
+    try
+    {
+        while (!std::cin.eof())
+        {
             new_line += 1;
-            if(getline(std::cin, line)) { 
+            if (getline(std::cin, line))
+            {
                 row += 1;
                 Lexer.tokenize(row, line);
             }
         }
-    } 
-    catch(const std::runtime_error& e){
+    }
+    catch (const std::runtime_error &e)
+    {
         std::cout << e.what() << std::endl;
         exit(1);
     }
-    if(new_line > row) {
+    if (new_line > row)
+    {
         Lexer.tokenList.push_back(Tokens(new_line, 1, "END"));
     }
-    else {
-        Lexer.tokenList.push_back(Tokens(row, Lexer.tokenList.back().col+1, "END"));
+    else
+    {
+        Lexer.tokenList.push_back(Tokens(row, Lexer.tokenList.back().col + 1, "END"));
     }
-    
 
-    //go through token list
-    //add to some string, if end, then 
-     // std::cout << "tokenlist size = " << Lexer.tokenList.size() << std::endl;
-     // std::cout << "tokenlist size = " << Lexer.tokenList.size() << std::endl;
-     std::vector<std::vector<Tokens>> tokensByLine;
+    // go through token list
+    // add to some string, if end, then
+    //  std::cout << "tokenlist size = " << Lexer.tokenList.size() << std::endl;
+    //  std::cout << "tokenlist size = " << Lexer.tokenList.size() << std::endl;
+    std::vector<std::vector<Tokens>> tokensByLine;
     std::vector<Tokens> temp;
     size_t index = 0;
     while (index + 1 < Lexer.tokenList.size())
@@ -111,87 +116,100 @@ int main() {
         }
     }
 
-    std::vector<Blocks*> astNodes;
+    std::vector<Blocks *> astNodes;
     size_t lineIndex = 0, tokenIndex = 0;
-    while (lineIndex < tokensByLine.size()) {
-        Blocks* astNode = nullptr;
+    while (lineIndex < tokensByLine.size())
+    {
+        Blocks *astNode = nullptr;
         astNode = parseStatements(tokensByLine, lineIndex, tokenIndex);
         // if (astNode) {
-            astNodes.push_back(astNode);
-            // lineIndex++;
+        astNodes.push_back(astNode);
+        // lineIndex++;
         // }
     }
 
-    for (Blocks* rootBlock : astNodes) {
+    for (Blocks *rootBlock : astNodes)
+    {
         printAST(rootBlock);
     }
-
 
     return 0;
 }
 
 // Implement this function to build the AST from the tokens
-Blocks* buildAST(const std::vector<Tokens>& /* tokens */) {
+Blocks *buildAST(const std::vector<Tokens> & /* tokens */)
+{
     // Add your logic to build the AST from the tokens
-    return new Blocks();  // Placeholder return
+    return new Blocks(); // Placeholder return
 }
 
 // Recursive function to print the AST with indentation
-void printAST(const Blocks* block, int indent) {
-    if (!block) return;
+void printAST(const Blocks *block, int indent)
+{
+    if (!block)
+        return;
 
     std::string indentation(indent, ' ');
 
-    if (block->type == "if" || block->type == "while") {
+    if (block->type == "if" || block->type == "while")
+    {
         std::cout << indentation << block->type << " (";
         printExpression(block->condition);
         std::cout << ") {" << std::endl;
 
-        for (const auto& childBlock : block->blocklist) {
+        for (const auto &childBlock : block->blocklist)
+        {
             printAST(childBlock, indent + 4);
         }
         std::cout << indentation << "}" << std::endl;
-    } else if (block->type == "print" || block->type == "expression") {
+    }
+    else if (block->type == "print" || block->type == "expression")
+    {
         std::cout << indentation << block->type << " ";
         printExpression(block->condition);
         std::cout << std::endl;
-    } else if (block->type == "else") {
+    }
+    else if (block->type == "else")
+    {
         // Handle the else block
         std::cout << indentation << "else {" << std::endl;
-        for (const auto& childBlock : block->blocklist) {
+        for (const auto &childBlock : block->blocklist)
+        {
             printAST(childBlock, indent + 4);
         }
         std::cout << indentation << "}" << std::endl;
     }
 }
 
-
 // Implement this function based on your ExpressionParser and how it represents expressions
-void printExpression( ExpressionParser* exp) {
+void printExpression(ExpressionParser *exp)
+{
     // Your logic to traverse and print the expression
-    ExpressionNode* root = exp->parseExpression();
-    if (root!=nullptr){
+    ExpressionNode *root = exp->parseExpression();
+    if (root != nullptr)
+    {
         root->getVariablesNames();
+        root->computeInfix();
         root->printInfix();
         delete root;
     }
-
-
 }
-bool checkoperator(std::string op){
+bool checkoperator(std::string op)
+{
     std::vector<std::string> ops = {
-    "+", "-", "*", "/", "=", "%",
-    "==", ">", ">=", "<", "<=", "|", "^", "&", "!=",
-    "+", "-", "*", "/", "=", "%", "==", ">", 
-    ">=", "<", "<=", "|", "^", "&", "!=",
-    "+", "-", "*", "/", "=", "%", "==", ">", ">=",
-     "<", "<=", "|", "^", "&", "!="};
-    if (std::find(ops.begin(), ops.end(), op) == ops.end()) {
+        "+", "-", "*", "/", "=", "%",
+        "==", ">", ">=", "<", "<=", "|", "^", "&", "!=",
+        "+", "-", "*", "/", "=", "%", "==", ">",
+        ">=", "<", "<=", "|", "^", "&", "!=",
+        "+", "-", "*", "/", "=", "%", "==", ">", ">=",
+        "<", "<=", "|", "^", "&", "!="};
+    if (std::find(ops.begin(), ops.end(), op) == ops.end())
+    {
         return false;
     }
     return true;
 }
-//gonna have to change both the below statements later
+// gonna have to change both the below statements later
 Blocks *parseStatements(std::vector<std::vector<Tokens>> &lines, size_t &lineIndex, size_t &tokenIndex)
 {
     if (lineIndex >= lines.size())
